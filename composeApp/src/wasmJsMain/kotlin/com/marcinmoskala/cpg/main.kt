be4adb588ebd98cd1ef47cb9d63b2a1c1382cbd4
Marcin Moskała <marcinmoskala@gmail.com>
2024-12-16 10:54:16 +0100

    Add loading spinner

--- composeApp/src/wasmJsMain/kotlin/com/marcinmoskala/cpg/main.kt
+++ composeApp/src/wasmJsMain/kotlin/com/marcinmoskala/cpg/main.kt
@@ -4,9 +4,14 @@ import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import com.marcinmoskala.composeexercises.ui.samples.guesser.GuesserScreen
 import kotlinx.browser.document
+import org.w3c.dom.asList
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
+    document.getElementById("no_wasm_comment")?.remove()
+    document.getElementById("spinner")?.remove()
+    document.body?.children?.asList()?.forEach { it.remove() }
+
     ComposeViewport(document.body!!) {
         LoadFont {
             GuesserScreen()
